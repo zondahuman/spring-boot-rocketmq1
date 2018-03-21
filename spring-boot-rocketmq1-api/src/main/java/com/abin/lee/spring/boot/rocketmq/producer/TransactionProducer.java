64e79a358f11b37a03e9a77c0ba038447b888bd7
zondahuman <zondahuman@gmail.com>
2018-03-21 17:40:22 +0800

    add

--- spring-boot-rocketmq1-api/src/main/java/com/abin/lee/spring/boot/rocketmq/producer/TransactionProducer.java
+++ spring-boot-rocketmq1-api/src/main/java/com/abin/lee/spring/boot/rocketmq/producer/TransactionProducer.java
@@ -28,7 +28,8 @@ public class TransactionProducer {
         producer.setCheckRequestHoldMax(200);
         producer.setTransactionCheckListener(transactionCheckListener);
 //        producer.setNamesrvAddr("172.16.2.133:9876");
-        producer.setNamesrvAddr("localhost:9876");
+        producer.setNamesrvAddr("127.0.0.1:9876");
+//        producer.setNamesrvAddr("localhost:9876");
 //        producer.setNamesrvAddr("zoo2:9876");
         producer.start();
 
